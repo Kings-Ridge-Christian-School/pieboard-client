b2f354c0cb89d4e0d9c863c8e5a26ddf4ddfdede
Lukas Holliger <14064434+lholliger@users.noreply.github.com>
2020-01-23 14:42:30 -0500

    show slides, cleanup

--- app.js
+++ app.js
@@ -64,15 +64,35 @@ function exists(path) {
         });
     });
 }
-
+function delFile(path) { 
+    return new Promise(async (resolve, reject) => {
+        fs.unlink(path, (err) => {
+            err ? reject(err) : resolve()
+        })
+    });
+}
+function cleanImages() {
+    let tmp_hash = []
+    for (image in manifest.data) {
+        tmp_hash.push(manifest.data[image].hash + ".b64")
+    }
+    fs.readdir(img_path, async (err, files) => {
+        for (file in files) {
+            if (!tmp_hash.includes(files[file])) {
+                await delFile(img_path + files[file])
+                console.log(`Deleted ${files[file]}`)
+            }
+        }
+    });
+}
 async function processManifest() {
     for (slide in manifest.data) {
-        if (await exists(img_path + manifest.data[slide].hash)) {
+        if (await exists(img_path + manifest.data[slide].hash + ".b64")) {
             console.log(manifest.data[slide].hash + " already saved")
         } else {
             let data = await get(`http://${manifest.address}:3000/api/slide/get/${manifest.data[slide].id}`)
             if (md5(data) == manifest.data[slide].hash) {
-                fs.writeFile(img_path + manifest.data[slide].hash, data, (err) => {
+                fs.writeFile(img_path + manifest.data[slide].hash + ".b64", data, (err) => {
                     if (err) console.log(err)
                     console.log("saved " + manifest.data[slide].hash)
                 });
@@ -86,12 +106,13 @@ app.get('/manifest', (req, res) => {
     res.send({"nonce": manifest.nonce})
 });
 
-app.post("/manifest", (req, res) => {
-    console.log(req.body.auth, auth)
+app.post("/manifest", async (req, res) => {
     if (req.body.auth == auth) {
         manifest = req.body
+        lock = manifest.nonce
         console.log("Loaded new manifest");
-        processManifest()
+        await processManifest()
+        await cleanImages()
         res.send({res: 0})
     }
 });
@@ -100,17 +121,27 @@ internal_app.get("/", (req, res) => res.sendFile(dir + "index.html"))
 internal_app.get("/ping", (req, res) => res.send({"nonce": lock}));
 internal_app.get("/manifest", (req, res) => res.send(manifest))
 internal_app.get("/index.js", (req, res) => res.sendFile(dir + "index.js"))
-internal_app.get("/image/:hash", (req, res) => res.sendFile(img_path + req.params.hash))
+internal_app.get("/image/:hash", (req, res) => res.sendFile(img_path + req.params.hash + ".b64"))
 
-
-if (manifest.id != null) {
-    get(`http://${manifest.address}:3000/dapi/refresh/${manifest.id}`);
-    console.log("Refreshing manifest");
-} else {
-    console.log("No manifest ID loaded, cannot refresh");
+async function checkForUpdate() {
+    if (manifest.id != null) {
+        let serverNonce = await get(`http://${manifest.address}:3000/dapi/getnonce/${manifest.id}`)
+        if (JSON.parse(serverNonce).nonce != manifest.nonce) {
+            get(`http://${manifest.address}:3000/dapi/refresh/${manifest.id}`);
+            console.log("Refreshing manifest");
+        } else {
+            console.log("Manifest up to date");
+        }
+    } else {
+        console.log("No manifest ID loaded, cannot refresh");
+    }
 }
 
-
 app.listen(port, () => console.log(`PieBord Client listening on port ${port}!`))
-internal_app.listen(local_port, 'localhost', () => console.log(`PieBoard Local Client listening on port ${local_port}!`))
+internal_app.listen(local_port, 'localhost', async () => {
+    console.log(`PieBoard Local Client listening on port ${local_port}!`)
+    await fs.promises.mkdir('data/img', { recursive: true })
+    await checkForUpdate()
+    setInterval(checkForUpdate, 60*60*1000);
+});
 
